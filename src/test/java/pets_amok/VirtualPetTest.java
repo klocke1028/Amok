518709f6c3ddeb7b2480bd137a5525008ca00159
klocke1028 <k.locke1028@gmail.com>
2024-06-27 00:34:13 -0400

    Still tweaking

--- src/test/java/pets_amok/VirtualPetTest.java
+++ src/test/java/pets_amok/VirtualPetTest.java
@@ -1,16 +1,29 @@
 package pets_amok;
 
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class VirtualPetTest {
-    private VirtualPet virtualPet;
 
+    private VirtualPetShelter shelter;
 
-
-        
-
+    @BeforeEach
+    public void setUp() {
+        shelter = new VirtualPetShelter();
     }
 
+    @Test
+    public void testAdmitPets() {
+        VirtualPet orgPet = new OrganicCat("TestCat", "Test org cat", 10, 10, 10, 10);
+        shelter.admitPet(orgPet);
+        assertEquals(2, 2);
+    }
 
+    @Test 
+    public void testPetExists() {
+        
+    }
 
+}
